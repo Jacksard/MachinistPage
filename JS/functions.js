0f9b99d673a1194058ada7b7e8bd7ccbddc29cf2
jacobgoskha <sardaukarkk@gmail.com>
2017-08-31 10:09:17 -0400

    simple validation applied to accept only numbers

--- JS/functions.js
+++ JS/functions.js
@@ -1,52 +1,64 @@
 function drillDepth () {
-		 var drillsize = document.getElementById('diameter').value;
-		 var atan = Math.tan(59);
-		 drillsize = drillsize/2;
-		 var depth = drillsize/atan;
-		 depth = depth/2;
-
-		 depth = depth.toFixed(4);     //limits to 4 dicemals after the point.
 		
-	document.getElementById("drillDepthAnswer").innerHTML="Drill depth is: "+depth+"\"";    //posts the calculation into the <p "id=drillDepthAnswer"> tag
-	
+		 var drillsize = document.getElementById('diameter').value;
+		 console.log(drillsize);
+		 if (isNaN(drillsize)) { 
+			document.getElementById("drillDepthAnswer").innerHTML="Enter a Number";	
+		 } else {
+			var atan = Math.tan(59);
+			drillsize = drillsize/2;
+
+			var depth = drillsize/atan;
+			depth = depth/2;
+			depth = depth.toFixed(4);     //limits to 4 dicemals after the point.
+			console.log(depth);
+			document.getElementById("drillDepthAnswer").innerHTML="Drill depth is: "+depth+"\"";    //posts the calculation into the <p "id=drillDepthAnswer"> tag
+		}
 
 	}
 
 
-function feed () {
+function feed() {
+	
 	var rpmc = document.getElementById('rpm').value;
 	var flutes = document.getElementById('flutes').value;
 	var fpt = document.getElementById('fpt').value;
+	
+	if ((isNaN(rpmc)) || (isNaN(flutes)) || (isNaN(fpt))) {
+		document.getElementById("feedAnswer").innerHTML="Enter Numbers";
+	} else {
 	var feedc = fpt * flutes * rpmc;
-
 	document.getElementById("feedAnswer").innerHTML="Feed : " +feedc+" inch/min";
+	}
 }
 
 function rpmMetric() {
 	var vc = document.getElementById('vc').value;
 	var dia = document.getElementById('diam').value;
+
+	if ((isNaN(vc)) || ((isNaN(dia)))) { 
+	document.getElementById("metricAnswer").innerHTML="Enter Numbers";
+	} else {
+
 	var pi = Math.PI;
 	
 	rpmm = (1000 * vc ) / (pi * dia) ;
-
 	rpmm = rpmm.toFixed(0);
 
 	document.getElementById("metricAnswer").innerHTML="RPM : " + rpmm;
+	}
 }
-
 
 function rpmImperial() {
 	var sfm = document.getElementById('sfm').value;
 	var diai = document.getElementById('diai').value;
 
-//	sfm=Number(sfm);
-//	diai=Number(diai);
-
+	if ((isNaN(sfm)) || (isNaN(diai))) {
+		document.getElementById("imperialAnswer").innerHTML="Enter Numbers";
+	} else {	
 	var rpmi = (sfm * 3.82) / diai;
-
-	rpmi = rpmi.toFixed(2);
-
+ 	rpmi = rpmi.toFixed(2);
 	document.getElementById("imperialAnswer").innerHTML="RPM : " + rpmi;
+	}
 }
-
 
